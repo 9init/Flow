0fc428c1cadeec1da4d4d14743f89f62bfa63c46
9init <emovent2e@gmail.com>
2020-02-07 21:40:54 +0200

    Server

--- go-server-side/SendEmail.go
+++ go-server-side/SendEmail.go
@@ -4,7 +4,6 @@ import (
 	"net/smtp"
 )
 
-// smtpServer data to smtp server
 type smtpServer struct {
 	host string
 	port string
@@ -15,21 +14,23 @@ func (s *smtpServer) Address() string {
 	return s.host + ":" + s.port
 }
 
-func sendEmail(emailAddress, message string) error {
-	from := "emovent2e@gmail.com"
-	password := "datwftuzeuooijom"
+//sendEmail is a function to send emails to client
+//emailAddress is the client's email, subject is the subject of the email and message is the message to send and it could be an html file
+func sendEmail(emailAddress, subject, message string) error {
+	from := "emovent.sup@gmail.com"
+	password := "zetfxupqlpeucdux"
 	to := emailAddress
 
 	//prepare hosting server
 	_smtpServer := smtpServer{host: "smtp.gmail.com", port: "587"}
 
-	msg := "From: " + "Ahmed Hesham" + "\n" +
+	msg := "From: " + "Emovent\"" + "\n" +
 		"To: " + to + "\n" +
-		"Subject: Test\n\n" +
+		"Subject: " + subject + "\n\n" +
 		message
 
 	//Authintication
-	auth := smtp.PlainAuth("", from, password, _smtpServer.host)
+	auth := smtp.PlainAuth("Emovent", from, password, _smtpServer.host)
 
 	//Sending Email
 	err := smtp.SendMail(_smtpServer.Address(), auth, from, []string{to}, []byte(msg))
